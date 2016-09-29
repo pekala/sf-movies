240d7ca5bab31ff3f337e3b3ba5fb6b444b8c717
Maciej Pekala <mp@issuu.com>
2016-09-29 20:53:00 +0200

    Add storybook for easy component development

---
+++ client/src/stories/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+
+storiesOf('empty', module)
+  .add('story', () => (
+    <div>Nothing to see here yet...</div>
+  ));
