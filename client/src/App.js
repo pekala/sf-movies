5b2b03610df3ccfffcd2d7e6d66380be2a43121a
Maciej Pekala <mp@issuu.com>
2016-10-02 19:09:56 +0200

    Add constants for timeouts

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import Result from './components/Result';
 import stateReducer from './state-reducer';
 import './App.css';
 
+const GAME_TICK_MS = 1000;
+const RESULT_DURATION_MS = 3000;
+
 class App extends Component {
     constructor() {
         super();
@@ -15,13 +18,13 @@ class App extends Component {
             if (this.state.question && !this.state.question.timeLeft) {
                 return this.onAnswer(null);
             }
-        }, 1000);
+        }, GAME_TICK_MS);
     }
     onAnswer(answer) {
         this.setState(state => stateReducer(state, 'ANSWER', answer));
         window.setTimeout(() => {
             this.setState(state => stateReducer(state, 'RESULT_SHOWN'));
-        }, 4000);
+        }, RESULT_DURATION_MS);
     }
     render() {
         const { question, result } = this.state;
