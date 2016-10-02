ba6f951d73a8fd6f7ec4a891097d167c70bd0975
Maciej Pekala <mp@issuu.com>
2016-10-02 17:15:52 +0200

    Integrate state reducer in the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Question from './components/Question';
+import stateReducer from './state-reducer';
 import './App.css';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = stateReducer(null, 'INIT');
+        window.setInterval(() => {
+            this.setState(state => stateReducer(state, 'TICK'));
+        }, 1000);
+    }
     render() {
         return (
             <div className="App">
+                <Question {...this.state.question} />
             </div>
         );
     }
