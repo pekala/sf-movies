f5d657894357d96feb74064e0b49c949c8c760f5
Maciej Pekala <mp@issuu.com>
2016-10-05 22:54:04 +0200

    Randomize the result messages

--- client/src/components/Result/Result.js
+++ client/src/components/Result/Result.js
@@ -2,14 +2,39 @@ import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 import './Result.css';
 
+const correctMessages = [
+    'Nice!',
+    'Woot Woot!',
+    'Great job!',
+    'Perfect!',
+    'You nailed it!',
+];
+
+const incorrectMessages = [
+    'Try harder next time!',
+    'Not even close...',
+    'Wait... what?',
+    'Huh...?',
+    'Sorry, incorrect.',
+];
+
+const timeoutMessages = [
+    'You need to be faster!',
+    'Next time, pick something...',
+    'Are you a chicken? Pick something!',
+    'Not enough hints, ha?',
+];
+
+const getRandom = messages => messages[Math.floor(Math.random() * messages.length)];
+
 const Question = ({ correct, points, hasTimedOut }) =>
     <div className={classNames('Result', { 'Result__incorrect': !correct })}>
         <div className="Result--header">
             {correct
-                ? 'Nice!'
+                ? getRandom(correctMessages)
                 : hasTimedOut
-                    ? 'You need to be faster!'
-                    : 'Try harder next time!'
+                    ? getRandom(timeoutMessages)
+                    : getRandom(incorrectMessages)
             }
         </div>
         {!hasTimedOut &&
